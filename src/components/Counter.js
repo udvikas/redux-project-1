68fae8eb2cb5e5acf89a19bd036129053b32072c
Vikash Sharma <kvikashsharma09@gmail.com>
2023-06-28 13:46:56 +0530

    integrating redux with react

---
+++ src/components/Counter.js
@@ -0,0 +1,42 @@
+import classes from './Counter.module.css';
+import { useSelector, useDispatch } from 'react-redux'; 
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const toggleCounterHandler = () => {};
+  const counter = useSelector(state => state.counter);
+
+  const incrementHandler = () => {
+    dispatch({ type: 'increment'})
+  };
+
+  const decrementHandler = () => {
+    dispatch ({ type: 'decrement'})
+  };
+  
+  const incrementBy2 = () => {
+    dispatch( {type: 'INCREMENTBY2'})
+  }
+
+  const decrementBy2 = () => {
+    dispatch( {type: 'DECREMENTBY2'})
+  }
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <div>
+        <button onClick={incrementBy2}>Increment by 2</button>
+        <button onClick={decrementBy2}>Decrement by 2</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
