e4a2b49a69aef52b93f2588dff5c56750e995a3d
Vikash Sharma <kvikashsharma09@gmail.com>
2023-06-29 17:34:51 +0530

    Lets Optimize and Do it the right way

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,49 +1,53 @@
+import { counterActions } from '../store/index';
 import classes from './Counter.module.css';
 import { useSelector, useDispatch } from 'react-redux'; 
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const toggleCounterHandler = () => {};
   const counter = useSelector(state => state.counter);
+  const show = useSelector(state => state.showCounter); 
 
   const incrementHandler = () => {
-    dispatch({ type: 'increment'})
+    dispatch(counterActions.increment())
   };
 
   const decrementHandler = () => {
-    dispatch ({ type: 'decrement'})
+    dispatch (counterActions.decrement())
   };
   
   const incrementBy2 = () => {
-    dispatch( {type: 'INCREMENTBY2'})
+    dispatch(counterActions.INCREMENTBY2(2)) // { type: 'SOME UNIQUE IDENTIFIER', payload: 2}
   }
 
   const decrementBy2 = () => {
-    dispatch( {type: 'DECREMENTBY2'})
+    dispatch(counterActions.DECREMENTBY2(2))
   }
 
-  const incrementBy5 = () => {
-    dispatch( {type: 'incrementby5'})
-  }
-  const decrementBy5 = () => {
-    dispatch( {type: 'decrementby5'})
+  // const incrementBy5 = () => {
+  //   dispatch( {type: 'incrementby5'})
+  // }
+  // const decrementBy5 = () => {
+  //   dispatch( {type: 'decrementby5'})
+  // }
+  const toggleCounterHandler = () => {
+    dispatch( counterActions.toggleCounter());
   }
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
-      {/* <div>
+      {show && <div className={classes.value}>{counter}</div>}
+       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={decrementHandler}>Decrement</button>
-      </div>
-      <div>
+      </div> 
+       <div>
         <button onClick={incrementBy2}>Increment by 2</button>
         <button onClick={decrementBy2}>Decrement by 2</button>
-      </div> */}
-      <div>
+      </div>  
+      {/* <div>
         <button onClick={incrementBy5}>IncrementBy5</button>
         <button onClick={decrementBy5}>DecrementBy5</button>
-      </div>
+      </div> */}
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,42 +1,82 @@
-import { createStore } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
+// import { createStore } from 'redux';
 
 
-const counterReducer = ( state={counter: 0}, action ) => {
+const initialState = {counter: 0, showCounter: true};
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers:{
+        increment (state) {
+            state.counter++;
+        },
+        decrement (state) {
+            state.counter--;
+        },
+        INCREMENTBY2(state,action) {
+            state.counter = state.counter + action.payload
+        },
+        DECREMENTBY2(state,action) {
+            state.counter = state.counter - action.payload
+        },
+        toggleCounter (state) {
+            state.showCounter = !state.showCounter
+        }
+    }
+});
+// const counterReducer = ( state= initialState, action ) => {
     
-    if(action.type === 'increment'){
-        return {
-            counter:state.counter + 1
-        }
-    }
-    if(action.type === 'decrement'){
-        return {
-            counter:state.counter - 1
-        }
-    }
-    if(action.type === 'INCREMENTBY2'){
-        return {
-            counter:state.counter + 2
-        }
-    }
-    if(action.type === 'DECREMENTBY2'){
-        return {
-            counter:state.counter - 2
-        }
-    }
-    if(action.type === 'incrementby5'){
-        return {
-            counter:state.counter + 5
-        }
-    }
-    if(action.type === 'decrementby5'){
-        return {
-            counter:state.counter - 5
-        }
-    }
-    return state;
-}
+//     if(action.type === 'increment'){
+//         return {
+//             counter:state.counter + 1,
+//             showCounter: state.showCounter,
+//         }
+//     }
+//     if(action.type === 'decrement'){
+//         return {
+//             counter:state.counter - 1,
+//             showCounter: state.showCounter,
+//         }
+//     }
+//     if(action.type === 'INCREMENTBY2'){
+//         return {
+//             counter:state.counter + action.amount,
+//             showCounter: state.showCounter,
+//         }
+//     }
+//     if(action.type === 'DECREMENTBY2'){
+//         return {
+//             counter:state.counter - 2,
+//             showCounter: state.showCounter,
+//         }
+//     }
+//     if(action.type === 'incrementby5'){
+//         return {
+//             counter:state.counter + 5,
+//             showCounter: state.showCounter,
+//         }
+//     }
+//     if(action.type === 'decrementby5'){
+//         return {
+//             counter:state.counter - 5,
+//             showCounter: state.showCounter,
+//         }
+//     }
 
-const store = createStore(counterReducer);
+//     if(action.type === 'toggle') {
+//         return {
+//             showCounter: !state.showCounter,
+//             counter:state.counter,
+//         }
+//     }
+//     return state;
+// }
 
+const store = configureStore({
+    reducer: counterSlice.reducer
+});
+
+export const counterActions = counterSlice.actions;
 export default store;
 
